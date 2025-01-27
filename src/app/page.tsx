115cf75541177abeeb551b8c0ddad598edd5f4ae
MaheshM-Velvetbud <mahesh.m@velvetbud.in>
2025-01-27 15:07:49 +0530

    Added hero,feature,service,section pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Hero from './components/Hero/Hero'
+import Feature from "./components/Feature/Feature"
+import Section from "./components/Section/Index"
+import Service from "./components/Service/Service"
 
 const page = () => {
   return (
     <div>
 <Hero/>
+<Section/>
+<Feature/>
+<Service/>
     </div>
   )
 }
